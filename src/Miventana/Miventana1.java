8c1a478e5a473ca30c3354e3f0f17218dbb5995e
paolaandyco71 <paolaandyco71@gmail.com>
2017-11-08 21:58:45 -0300

    CREACION SWING

---
+++ src/Miventana/Miventana1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Miventana;
+//PASO 1 - IMPORTAR LAS LIBRERIAS
+import javax.swing.*;//Importamos TODO de swing
+import java.awt.*;
+
+
+
+
+/**
+ *
+ * @author LAB03
+ */
+public class Miventana1 
+{
+    
+    
+}
---
+++ src/javaswing/JavaSwing.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaswing;
+//PASO 1 - IMPORTAR LAS LITERARIAS
+import javax.swing.*;//Importamos TODO de swing
+import java.awt.*;
+/**
+ *
+ * @author LAB03
+ */
+//PASO 2 - HEREDAR LA CLASE DESDE FRAME
+public class JavaSwing
+        
+        
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+
+        
+    }
+    
+}
